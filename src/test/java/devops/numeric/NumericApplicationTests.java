a6dc26ef864fa006197d8f8a44ab454fe27ae62e
Mohamed Ahmed <softtechsd@gmail.com>
2023-07-25 14:38:35 +0300

    add pom.xml

---
+++ src/test/java/devops/numeric/NumericApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.numeric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumericApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
